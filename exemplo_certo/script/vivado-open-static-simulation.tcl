68b06db915a60c6c458d3ea19e85da992c45a806
CarlosNatham <50326473+CarlosNatham@users.noreply.github.com>
2022-10-11 13:42:24 -0300

    Delete exemplo_certo directory

--- exemplo_certo/script/vivado-open-static-simulation.tcl
+++
@@ -1,7 +0,0 @@
-set BASE_DIR     [lindex $argv 0]
-set WORK_DIR     [lindex $argv 1]
-
-# open static simulation
-open_wave_database $WORK_DIR/xilinx/harv-soc-ahx-sim.sim/sim_1/behav/xsim/ahx_tb_behav.wdb
-# open wave configuration
-open_wave_config $BASE_DIR/utils/script/simulation/ahx_tb_behav.wcfg
