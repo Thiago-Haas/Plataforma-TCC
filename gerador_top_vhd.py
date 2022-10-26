314aaeb4e9a7bf2f16b80e460efd61a9b38177ec
Douglas Almeida dos Santos <douglas.almd.santos@gmail.com>
2022-10-26 18:55:54 +0200

    Move compressor to hdl folder

--- gerador_top_vhd.py
+++ gerador_top_vhd.py
@@ -1300,7 +1300,7 @@ class Gerador_Vhdl(object):
         os.makedirs(diretorio_sim)
         os.makedirs(diretorio_hdl)
         shutil.copytree('harv-soc', os.path.join(diretorio, 'harv-soc'), dirs_exist_ok = True)
-        shutil.copytree('compressor_axi', os.path.join(diretorio, 'compressor'), dirs_exist_ok = True)
+        shutil.copytree('compressor_axi', os.path.join(diretorio_hdl, 'compressor'), dirs_exist_ok = True)
         shutil.copytree('arquivos_topo/fpga', os.path.join(diretorio, 'fpga'), dirs_exist_ok = True)
         shutil.copy('arquivos_topo/vivado-ahx-sim.tcl', diretorio_script)
         shutil.copy('arquivos_topo/vivado-open-static-simulation.tcl', diretorio_script)
