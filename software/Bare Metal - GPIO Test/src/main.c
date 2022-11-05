db919a4f595ec243006e29488b3bf4925afaa8b1
Douglas Almeida dos Santos <douglas.almd.santos@gmail.com>
2022-11-05 16:39:27 +0100

    Improvements in GPIO test software

--- software/Bare Metal - GPIO Test/src/main.c
+++ software/Bare Metal - GPIO Test/src/main.c
@@ -4,24 +4,11 @@
 #include <harvsoc.h>
 #include <csr.h>
 
-unsigned long trap_handler(
-    unsigned long mepc,
-    unsigned long mtval,
-    unsigned long mcause,
-    unsigned long mhartid,
-    unsigned long mstatus,
-    unsigned long sp
-) {
-    if (mcause >> 31) {
-      // is interrupt
-      printf("interrupt 0x%08X\n", mcause);
-    } else {
-      // is exception
-      printf("exception 0x%08X\n", mcause);
-    }
-    return mepc;
-}
-
+#ifndef IS_SIMULATION
+#define DELAY_COUNT 100000
+#else
+#define DELAY_COUNT 100
+#endif
 int main() {
     set_harden_conf(0x37);
     uart_init(434, 0, 1); // 115200 baud rate
@@ -32,21 +19,12 @@ int main() {
 
     while (1) {
         // delay
-        for (int i = 0; i < 100000; i++)
+        for (int i = 0; i < DELAY_COUNT; i++) {
             wdt_feed();
-        gpio_write(8, 0);
-        gpio_write(7, 0);
-        gpio_write(6, 0);
-        gpio_write(5, 0);
-        gpio_write(4, 1);
-        gpio_write(3, 1);
-        gpio_write(2, 1);
-        gpio_write(1, 1);
-        for (int i = 8; i >= 0; i--)
-            printf("[%d]: %x\n", i, gpio_read(i));
-        // delay
-        for (int i = 0; i < 100000; i++)
-            wdt_feed();
+        }
+
+        // write 1 in first 4 leds
+        printf("0\n");
         gpio_write(8, 1);
         gpio_write(7, 1);
         gpio_write(6, 1);
@@ -55,9 +33,37 @@ int main() {
         gpio_write(3, 0);
         gpio_write(2, 0);
         gpio_write(1, 0);
-        printf("1\n");
-        for (int i = 8; i >= 0; i--)
+
+        // read GPIO
+        for (int i = 8; i >= 0; i--) {
             printf("[%d]: %x\n", i, gpio_read(i));
+        }
+
+        // delay
+        for (int i = 0; i < DELAY_COUNT; i++) {
+            wdt_feed();
+        }
+
+        // write 1 in last 4 leds
+        printf("1\n");
+        gpio_write(8, 0);
+        gpio_write(7, 0);
+        gpio_write(6, 0);
+        gpio_write(5, 0);
+        gpio_write(4, 1);
+        gpio_write(3, 1);
+        gpio_write(2, 1);
+        gpio_write(1, 1);
+        
+        // read GPIO
+        for (int i = 8; i >= 0; i--) {
+            printf("[%d]: %x\n", i, gpio_read(i));
+        }
+
+#ifdef IS_SIMULATION
+        break;
+#endif
+
     }
 
     return 0;
